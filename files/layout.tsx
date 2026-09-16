904f4497ad174451f1b840c3e506d9b5d8dc1c2f
Theuri <theuri@kali.theuri>
2026-09-16 12:14:54 +0300

    second commit

--- files/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { MerchantShell } from "@/components/shell/merchant-shell";
-
-export default function MerchantLayout({ children }: { children: React.ReactNode }) {
-  return <MerchantShell>{children}</MerchantShell>;
-}
